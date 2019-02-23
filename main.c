0339beb7813c696052df62399e240772f0607921
Christian Bagdon <cbagdon119@gmail.com>
2019-02-22 17:32:13 -0800

    Error Checking Update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cbagdon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 19:21:17 by cbagdon           #+#    #+#             */
-/*   Updated: 2019/02/22 16:55:47 by cbagdon          ###   ########.fr       */
+/*   Updated: 2019/02/22 17:29:30 by cbagdon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,26 @@ int		main(int argc, char *argv[])
 	int		fd;
 	int		tetriminos;
 
-	(void)argc;
-	tetriminos = 0;
-	fd = open(argv[1], O_RDONLY);
-	if (validate_file(fd, 0, 0, &tetriminos))
+	if (argc == 2)
 	{
-		close(fd);
-		if (!make_array(tetriminos, argv[1]))
+		tetriminos = 0;
+		fd = open(argv[1], O_RDONLY);
+		if (validate_file(fd, 0, 0, &tetriminos))
+		{
+			close(fd);
+			if (!make_array(tetriminos, argv[1]))
+			{
+				ft_putstr("error\n");
+				return (1);
+			}
+		}
+		else
 		{
 			ft_putstr("error\n");
-			return (0);
+			return (1);
 		}
 	}
 	else
-		ft_putstr("error\n");
+		ft_putstr("usage: fillit input_file\n");
 	return (0);
 }
